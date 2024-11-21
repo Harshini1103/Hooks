502d3638786d08e82765d379d0b74ffbd84f59e3
Harshinin P <harshini11122003@gmail.com>
2024-11-21 14:54:13 +0530

    Update useEffect.jsx
    It is very easy to understand the concept of useEffect Hook

--- useEffect.jsx
+++ useEffect.jsx
@@ -1,1 +1,28 @@
+import React, { useEffect, useState } from 'react';
+function LifeCycle(){
+    const [count1,setCount1]=useState(0);
+    const [count2,setCount2]=useState(0)
+    // type-1
+    // useEffect(()=>{
+    //     console.log("Use Effect Executing!");
+        
+    // })
+    //type-2
+//     useEffect(()=>{
+// console.log("Use Efffect Executing");
+
+//     },[count1])
+// type-3
+useEffect(()=>{
+    console.log("Use eFFect Executing");
+    
+},[])
+    return<>
+    <h1>{count1}</h1>
+    <h2>{count2}</h2>
+    <button onClick={()=>setCount1(count1+1)}>Click This</button>
+    <button onClick={()=>setCount2(count2+1)}>Click This</button>
+    </>
+}
+export default LifeCycle;
 
